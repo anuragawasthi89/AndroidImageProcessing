a88e404086ba107afb69a6ec811d73d389c20ff9
anuragawasthi89 <anuragawasthi89@gmail.com>
2014-02-08 02:40:39 +0530

    Sample App set up with 2 activities and sample main window

--- CVProcessor/gen/com/example/cvprocessor/R.java
+++ CVProcessor/gen/com/example/cvprocessor/R.java
@@ -25,20 +25,23 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int exit_button=0x7f070004;
+        public static final int exit_button=0x7f070003;
         public static final int face_tracking_button=0x7f070002;
         public static final int fullscreen_content=0x7f070000;
-        public static final int fullscreen_content_controls=0x7f070003;
+        public static final int fullscreen_content_controls=0x7f070004;
+        public static final int main_menu_button=0x7f070005;
         public static final int view_camera_calibration_button=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main_window=0x7f030000;
+        public static final int activity_view_cam_calibrations=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int dummy_content=0x7f050002;
         public static final int exit_button=0x7f050001;
         public static final int face_tracking_button=0x7f050004;
+        public static final int main_menu_button=0x7f050005;
         public static final int view_camera_calibration_button=0x7f050003;
     }
     public static final class style {
